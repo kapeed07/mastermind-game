e8c5d84608b41873b26ea4c9bdd28237d3a2511c
Deepak Bhardwaj <kapeed07@gmail.com>
2021-05-01 23:38:35 +0530

    add: winning and losing images

--- src/App.js
+++ src/App.js
@@ -1,5 +1,9 @@
 import React, { useState } from 'react';
 import './app.scss';
+import Success from './success.svg';
+import GameOver from './game-over.svg';
+import ThumbsDown from './thumbs-down.svg';
+import Replay from './replay.svg';
 
 function App() {
   const [moves, setMoves] = useState([
@@ -43,12 +47,12 @@ function App() {
           <span className="text-yellow">d</span>
           <div className="spiral"></div>
         </p>
-        
         <div className="text-14">
           <p className="mb-6">Try to guess the pattern, in both order and color, within ten turns.</p>
           <p className="mb-6">After submitting a row, a small black peg is placed for each code peg from the guess which is correct in both color and position.</p>
           <p className="mb-6">A white peg indicates the existence of a correct color code peg placed in the wrong position. More info on <a className="text-purple" href="https://en.wikipedia.org/wiki/Mastermind_(board_game)">Wikipedia.</a></p>
         </div>
+        {/* <img className="width-100" src={GameOver} /> */}
         <div className="mt-30 flex items-start">
           <div className="color-pallet flex">
             {colorPallet.map(color =><div onClick={() => setActive(color)} className={`pallet bg-${color} ${active === color ? 'active' : ''}`}></div> )}
@@ -74,6 +78,19 @@ function App() {
               </div>
             )}
           </div>
+
+          
+          {/* <div>
+            <img className="width-100" src={ThumbsDown} />
+            <p className="mt-10 ml-10 text-red weight-700">Game Over</p>
+            <img className="ml-30 mt-20 width-40" src={Replay} />
+          </div> */}
+          {/* <div>
+            <img className="width-100" src={Success} />
+            <p className="mt-10 ml-10 text-green weight-700">You Won!!!</p>
+            <img className="ml-30 mt-20 width-40" src={Replay} />
+          </div> */}
+          
         </div>
       </div>
     </div>
